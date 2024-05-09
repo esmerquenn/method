c62c48539101cfc76d2061128bb84639ee882f56
esmerquenn <esmereyvazov@gmail.com>
2024-05-09 19:35:04 +0400

    about

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -66,30 +66,32 @@ function toggle(){
 //    };
 //  }
 
+document.addEventListener('scroll', function() {
+  if (window.innerWidth < 768) {
+    const images = document.querySelectorAll('.main_img, .main_img_vertical');
+    let closest = null;
+    let closestDist = Infinity;
 
- document.addEventListener('scroll', function() {
-   const images = document.querySelectorAll('.main_img, .main_img_vertical');
-   let closest = null;
-   let closestDist = Infinity;
- 
-   images.forEach(img => {
-     const imgRect = img.getBoundingClientRect();
-     const imgCenter = imgRect.top + (imgRect.height / 2);
-     const screenCenter = window.innerHeight / 2;
-     const dist = Math.abs(screenCenter - imgCenter);
- 
-     if (dist < closestDist) {
-       closest = img;
-       closestDist = dist;
-     }
-   });
- 
-   images.forEach(img => {
-     if (img === closest) {
-       img.style.filter = 'grayscale(0)';
-     } else {
-       img.style.filter = 'grayscale(2)';
-     }
-   });
- });
+    images.forEach(img => {
+      const imgRect = img.getBoundingClientRect();
+      const imgCenter = imgRect.top + (imgRect.height / 2);
+      const screenCenter = window.innerHeight / 2;
+      const dist = Math.abs(screenCenter - imgCenter);
+
+      if (dist < closestDist) {
+        closest = img;
+        closestDist = dist;
+      }
+    });
+
+    images.forEach(img => {
+      if (img === closest) {
+        img.style.filter = 'grayscale(0)';
+      } else {
+        img.style.filter = 'grayscale(100%)';  // Grayscale değeri 0 ile 100% arasında olmalıdır
+      }
+    });
+  }
+});
+
  
